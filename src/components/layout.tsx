d01767360bd3404ef9c3db1a2fd04681a758ad6d
Jakka Prihatna <jprihatna@gmail.com>
2025-07-20 14:15:16 +0700

    feat: add sidebar items
    Signed-off-by: Jakka Prihatna <jprihatna@gmail.com>

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,7 @@ import type { HTMLAttributes, PropsWithChildren, ReactNode } from 'react';
 
 import styles from './layout.module.scss';
 import { Sidebar } from '@/ui/sidebar';
-import { PlusIcon } from '@/ui/icons';
+import { ArchivedIcon, CompletedIcon, PinIcon, PlusIcon, TaskIcon, TodayIcon, UpcomingIcon } from '@/ui/icons';
 import { usePathname } from 'next/navigation';
 
 type LayoutProps = PropsWithChildren<HTMLAttributes<HTMLDivElement>> & {
@@ -32,12 +32,32 @@ const Layout = ({ className, footer, navbar, sidebar, children, ...props }: Layo
       {sidebar !== false
         ? <Sidebar className="layout-sidebar" collapsed={sidebarCollapsed} toggleSidebar={collapseSidebar}>
             <Sidebar.Menu header="Tasks">
+              <Sidebar.MenuItem active={pathname == '/'} icon={<TaskIcon />}  href="/" text="All" />
+              <Sidebar.MenuItem active={pathname == '/upcoming'} icon={<UpcomingIcon />}  href="/upcoming" text="Upcoming" />
+              <Sidebar.MenuItem active={pathname == '/today'} icon={<TodayIcon />} href="/today" text="Today" />
+              <Sidebar.MenuItem active={pathname == '/done'} icon={<CompletedIcon />} href="/done" text="Completed" />
+              <Sidebar.MenuItem active={pathname == '/pin'} icon={<PinIcon />} href="/pin" text="Pinned" />
+              <Sidebar.MenuItem active={pathname == '/archive'} icon={<ArchivedIcon />} href="/archive" text="Archived" />
             </Sidebar.Menu>
             <Sidebar.Menu header="List">
+              <Sidebar.MenuItem href="/personal" text="Personal" />
+              <Sidebar.MenuItem href="/work" text="Work" />
+              <Sidebar.MenuItem href="/health" text="Health" />
+              <Sidebar.MenuItem href="/shopping" text="Shopping" />
+              <Sidebar.MenuItem href="/misc" text="Other" />
               <Sidebar.MenuItem icon={<PlusIcon />} text="Add New List" />
             </Sidebar.Menu>
             <Sidebar.Menu header="Tags">
               <Sidebar.Tags>
+                <Sidebar.Tag text="Foo" />
+                <Sidebar.Tag text="Bar" />
+                <Sidebar.Tag text="Baz" />
+                <Sidebar.Tag text="What" />
+                <Sidebar.Tag text="Who" />
+                <Sidebar.Tag text="When" />
+                <Sidebar.Tag text="Where" />
+                <Sidebar.Tag text="Why" />
+                <Sidebar.Tag text="How" />
               </Sidebar.Tags>
               <Sidebar.MenuItem icon={<PlusIcon />} text="Add New Tag" />
             </Sidebar.Menu>
--- src/ui/icons.tsx
+++ src/ui/icons.tsx
@@ -2,10 +2,17 @@ import GitHubIcon from '@/ui/icons/github.svg';
 import UpRightIcon from '@/ui/icons/up-right.svg';
 import SettingsIcon from '@/ui/icons/settings.svg';
 import SignOutIcon from '@/ui/icons/sign-out.svg';
-import HashIcon from '@/ui/icons/hash.svg'
-import ListIcon from '@/ui/icons/list.svg'
-import PlusIcon from '@/ui/icons/plus.svg'
+import HashIcon from '@/ui/icons/hash.svg';
+import ListIcon from '@/ui/icons/list.svg';
+import PlusIcon from '@/ui/icons/plus.svg';
+import ArchivedIcon from '@/ui/icons/archived.svg';
+import UpcomingIcon from '@/ui/icons/upcoming.svg';
+import TodayIcon from '@/ui/icons/today.svg';
+import PinIcon from '@/ui/icons/pin.svg';
+import TaskIcon from '@/ui/icons/task.svg';
+import CompletedIcon from '@/ui/icons/completed.svg';
 
 export {
-  GitHubIcon, UpRightIcon, SignOutIcon, SettingsIcon, HashIcon, ListIcon, PlusIcon
+  GitHubIcon, UpRightIcon, SignOutIcon, SettingsIcon, HashIcon, ListIcon, PlusIcon,
+  ArchivedIcon, UpcomingIcon, TodayIcon, PinIcon, TaskIcon, CompletedIcon
 };
