5dce5d0d5eb52826d12870588d3c7c3d11d2df0b
Jakka Prihatna <jprihatna@gmail.com>
2025-07-22 18:30:29 +0700

    fix: progress calculation
    Signed-off-by: Jakka Prihatna <jprihatna@gmail.com>

--- src/components/page-title.tsx
+++ src/components/page-title.tsx
@@ -7,29 +7,30 @@ import { TodoSummary } from '@/state/todo/types';
 import { cx } from '@/ui/utils';
 
 import styles from './page-title.module.scss';
+import { resolveSlug } from '@/services/todo';
 
 type PageTitleProps = ComponentProps<'div'>;
 
-const getSummary = (summary: TodoSummary, type = 'all'): [number, number] => {
+const getProgress = (summary: TodoSummary, type = 'all'): [number, number] => {
   if (['upcoming', 'today', 'done', 'pin', 'all', 'archive'].includes(type)) {
-    return summary[type];
+    return summary[type] ?? [0,0];
   }
 
   return summary.list[type] ?? [0,0];
 }
 
 const PageTitle = ({ className, ...props }: PageTitleProps) => {
-  const params = useParams();
-  const type = Array.isArray(params.type) ? params.type.join('/') : params.type;
+  const { slug } = useParams();
+  const type = resolveSlug(slug);
   const { title, summary } = useAppSelector((state) => state.todos);
   const [completed, total] = useMemo(() => {
-    return summary ? getSummary(summary, type) : [0,0];
+    return summary ? getProgress(summary, type) : [0,0];
   }, [summary, type]);
 
   return (
     <div {...props} className={cx(styles.title, "page-title", className)}>
       <div>{titleCase(title)}</div>
-      { total > 0 ? <div className="count">{completed}<span className="page-title-separator">/</span>{total}</div> : null }
+      { total >= 0 ? <div className="count">{completed}<span className="page-title-separator">/</span>{total}</div> : null }
     </div>
   )
 }
